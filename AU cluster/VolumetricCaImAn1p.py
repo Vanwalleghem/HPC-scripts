545f057e241649da92a08ba6c21b4c9a407645c3
Gilles Vanwalleghem <snooles@hotmail.com>
2024-06-25 14:42:26 +0200

    Optimized volumetric CaImAn

--- AU cluster/VolumetricCaImAn1p.py
+++ AU cluster/VolumetricCaImAn1p.py
@@ -13,7 +13,8 @@ from caiman.source_extraction.cnmf import params as params
 import cv2
 from subprocess import call
 from caiman.utils.visualization import nb_view_patches3d
-
+import time
+opencv=True
 
 try:
     cv2.setNumThreads(0)
@@ -40,8 +41,8 @@ decay_time = 1.6                 # length of a typical transient in seconds
 # motion correction parameters
 motion_correct = True    # flag for performing motion correction
 pw_rigid = True         # flag for performing piecewise-rigid motion correction (otherwise just rigid)
-gSig_filt = (3, 3)       # size of high pass spatial filtering, used in 1p data
-max_shifts = (5, 5)      # maximum allowed rigid shift
+gSig_filt = None #(3, 3, 2)       # size of high pass spatial filtering, used in 1p data
+max_shifts = (5, 5, 2)      # maximum allowed rigid shift
 strides = (24, 24, 6)       # start a new patch for pw-rigid motion correction every x pixels
 overlaps = (12, 12, 2)      # overlap between pathes (size of patch strides+overlaps)
 max_deviation_rigid = 3  # maximum deviation allowed for patch with respect to rigid shifts
@@ -62,10 +63,21 @@ mc_dict = {
 
 opts = cnmf.params.CNMFParams(params_dict=mc_dict)
 
-mc = cm.motion_correction.MotionCorrect(FourD_File[0], dview=dview, **opts.get_group('motion'))
-mc.motion_correct(save_movie=True)
+if not glob.glob(os.path.join('/faststorage/project/FUNCT_ENS/CaImAnTemp/',os.path.basename(FourD_File[0]).replace('.tif','*.mmap'))):
+ mc = cm.motion_correction.MotionCorrect(FourD_File[0], dview=dview, **opts.get_group('motion'))
+ try:
+  mc.motion_correct(save_movie=True)
+ except:
+  time.sleep(10)
+  if not glob.glob(os.path.join('/faststorage/project/FUNCT_ENS/CaImAnTemp/',os.path.basename(FourD_File[0]).replace('.tif','*.mmap'))):
+   mc = cm.motion_correction.MotionCorrect(FourD_File[0], dview=dview, **opts.get_group('motion'))
 
-fname_new = cm.save_memmap(mc.mmap_file, base_name='memmap_', order='C',border_to_0=0, dview=dview) # exclude borders
+if glob.glob(os.path.join('/faststorage/project/FUNCT_ENS/CaImAnTemp/',os.path.basename(FourD_File[0]).replace('.tif','*.mmap'))):
+ fname_new = cm.save_memmap(mc.mmap_file, base_name='memmap_', order='C',border_to_0=0, dview=dview) # exclude borders
+else:
+ mc.motion_correct(save_movie=True)
+
+time.sleep(10)
 Yr, dims, T = cm.load_memmap(fname_new)
 images = np.reshape(Yr.T, [T] + list(dims), order='F') 
     #load frames in python format (T x X x Y)
@@ -73,60 +85,45 @@ images = np.reshape(Yr.T, [T] + list(dims), order='F')
 # set parameters
 rf = 25  # half-size of the patches in pixels. rf=25, patches are 50x50
 stride = 10  # amount of overlap between the patches in pixels
-K = 50  # number of neurons expected per patch
+K = 100  # number of neurons expected per patch
 gSig = [3, 3, 2]  # expected half size of neurons
-merge_thresh = 0.8  # merging threshold, max correlation allowed
+merge_thr = 0.8  # merging threshold, max correlation allowed
 p = 1  # order of the autoregressive system
 tsub = 2            # downsampling factor in time for initialization,
 ssub = 1            # downsampling factor in space for initialization,
-min_pnr = 5        # min peak to noise ration from PNR image
+min_pnr = 3        # min peak to noise ration from PNR image
 ssub_B = 3          # additional downsampling factor in space for background
 ring_size_factor = 1.4  # radius of ring is gSiz*ring_size_factor
 rval_thr = 0.6   # accept components with space correlation threshold or higher
 print('set')
 
-opts = cnmf.params.CNMFParams(params_dict={'method_init': 'corr_pnr',
+opts = cnmf.params.CNMFParams(params_dict={
                                 'K': K,
                                 'gSig': gSig,                                
                                 'merge_thr': merge_thr,
                                 'p': p,
                                 'tsub': tsub,
-                                'ssub': ssub,
-                                'rf': rf,
-                                'stride': stride,
+                                'ssub': ssub,                                
+                                'use_cnn':False,                                
                                 'only_init': True,    # set it to True to run CNMF-E                                                                
                                 'method_deconvolution': 'oasis',       # could use 'cvxpy' alternatively        
                                 'update_background_components': True,                                
                                 'min_pnr': min_pnr,
                                 'normalize_init': False,               # just leave as is
+                                'rval_thr': rval_thr,
                                 'center_psf': True,                    # leave as is for 1 photon
                                 'ssub_B': 3,
                                 'ring_size_factor': ring_size_factor,
                                 'del_duplicates': True,                # whether to remove duplicates from initialization
-                                'border_pix': 5})                # number of pixels to not consider in the borders)
+                                'border_pix': 0})                # The parameter border pix must be set to 0 for 3D data since border removal is not implemented)
 
-cnm = cnmf.CNMF(n_processes, 
-                k=K, 
-                gSig=gSig, 
-                merge_thresh=merge_thresh, 
-                p=p, 
-                dview=dview,
-                rf=rf, 
-                stride=stride, 
-                only_init_patch=True,
-                'min_pnr': min_pnr,
-                'center_psf': True,
-                'rval_thr': rval_thr,
-                'tsub': tsub,
-                'use_cnn':False,
-                'ssub': ssub
-                , params=opts)
+cnm = cnmf.CNMF(n_processes, k=K, gSig=gSig, merge_thresh=merge_thr, p=p,dview=dview,rf=rf,stride=stride,only_init_patch=True)
                 
-cnm.params.set('spatial', {'se': np.ones((3,3,1), dtype=np.uint8)})
+#cnm.params.set('spatial', {'se': np.ones((3,3,1), dtype=np.uint8)})
 cnm = cnm.fit(images)
 
-min_SNR = 3            # adaptive way to set threshold on the transient size
-r_values_min = 0.85    # threshold on space consistency (if you lower more components
+min_SNR = 2            # adaptive way to set threshold on the transient size
+r_values_min = 0.7    # threshold on space consistency (if you lower more components
 #                        will be accepted, potentially with worst quality)
 cnm.params.set('quality', {'min_SNR': min_SNR,'rval_thr': r_values_min,'use_cnn': False})
 cnm.estimates.evaluate_components(images, cnm.params, dview=dview)
@@ -139,7 +136,7 @@ try:
     cnm.estimates.detrend_df_f(quantileMin=5, frames_window=200)
 except:
     pass
-#cnm.save(fnames[0].replace('.tif','.hdf5'))
+cnm.save(FourD_File[0].replace('.tif','.hdf5'))
 cnm2 = cnm.refit(images, dview=dview)
 cnm2.estimates.evaluate_components(images, cnm2.params, dview=dview)
 
@@ -147,6 +144,6 @@ print(' ***** ')
 print(f"Number of total components: {len(cnm2.estimates.C)}")
 print(f"Number of accepted components: {len(cnm2.estimates.idx_components)}")
 
-cnm2.save(fnames[0].replace('.tif','.hdf5'))
+cnm2.save(FourD_File[0].replace('.tif','b.hdf5'))
 
 cm.stop_server(dview=dview)
